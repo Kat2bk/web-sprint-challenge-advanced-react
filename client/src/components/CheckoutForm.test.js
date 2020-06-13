b46f63b344de940090b9af7b802f60bdc333253b
Katherine-Kumar <Katherine2bk@gmail.com>
2020-06-13 10:15:11 -0700

    added second test, it passed. grabbed input labels, captured the change, expected the values to be those listed, checked button event, grabbed my name and checked to see if successMessage registered by testid... turns out I was mispelling testId as textId

--- client/src/components/CheckoutForm.test.js
+++ client/src/components/CheckoutForm.test.js
@@ -10,4 +10,36 @@ test("form header renders", () => {
     expect(header).toBeInTheDocument();
 });
 
-test("form shows success message on submit with form details", () => {});
+test("form shows success message on submit with form details", () => {
+    const {getByLabelText, getByText, getByTestId} = render(<CheckoutForm />);
+
+    const firstNameInput = getByLabelText(/first name/i);
+    const lastNameInput = getByLabelText(/last name/i);
+    const addressInput = getByLabelText(/address/i);
+    const cityInput = getByLabelText(/city/i);
+    const stateInput = getByLabelText(/state/i);
+    const zipInput = getByLabelText(/zip/i);
+
+    fireEvent.change(firstNameInput, {target: {value: 'kat'}});
+    fireEvent.change(lastNameInput, {target: {value: 'kumar'}});
+    fireEvent.change(addressInput, {target: {value: '587 Jeff'}});
+    fireEvent.change(cityInput, {target: {value: 'LC'}});
+    fireEvent.change(stateInput, {target: {value: 'LA'}});
+    fireEvent.change(zipInput, {target: {value: '7777'}});
+
+    expect(firstNameInput.value).toBe('kat');
+    expect(lastNameInput.value).toBe('kumar');
+    expect(addressInput.value).toBe('587 Jeff');
+    expect(cityInput.value).toBe('LC');
+    expect(stateInput.value).toBe('LA');
+    expect(zipInput.value).toBe('7777');
+
+    const submitButton = getByText('Checkout');
+    fireEvent.click(submitButton);
+
+    const getMyName = getByText(/kat/i);
+    expect(getMyName).toBeInTheDocument();
+
+    const successful = getByTestId(/successMessage/i);
+    expect(successful).toBeInTheDocument();
+});
